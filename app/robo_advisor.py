9a2f88088ce004833c1b5a75f8c30385c1bcefe5
kyokang1 <kyoungmokang@gmail.com>
2019-06-18 22:53:12 -0400

    high_prices & low_prices

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -21,6 +21,32 @@ def get_response(symbol):
     response_data = json.loads(response.text)    
     return response_data
 
+def transform_response(parsed_response):
+    tsd = parsed_response["Time Series (Daily)"]
+    rows = []
+    for date, daily_prices in tsd.items():
+        row = {
+            "date": date,
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"],
+            "close": daily_prices["4. close"],
+            "volume": daily_prices["5. volume"] 
+        }
+        rows.append(row)
+    return rows
+
+
+#    aapl = json.loads(file_contents)
+#    
+#    meta = aapl["Meta Data"]
+#    last_refreshed = meta["3. Last Refreshed"]
+#    
+#    daily = aapl["Time Series (Daily)"]
+#    latest_data = daily[str(last_refreshed)]
+#    latest_closing_price = latest_data["4. close"]
+#
+#    return latest_closing_price
 
 
 
@@ -40,6 +66,14 @@ symbol = input("Please input a stock symbol (e.g. MSFT, AAPL, AMZN): ")
 parsed_response = get_response(symbol)
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 #breakpoint()
+
+rows = transform_response(parsed_response)
+latest_close = rows[0]["close"]
+high_prices = [row["high"] for row in rows] #[team["name"] for team in teams]
+low_prices = [row["low"] for row in rows] #[team["name"] for team in teams]
+
+
+breakpoint()
 
 
 
