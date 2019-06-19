67fdb00c067dbc4299264c5b1ab8ed01406186cb
kyokang1 <kyoungmokang@gmail.com>
2019-06-18 22:43:40 -0400

    Output hardcoding

---
+++ app/robo_advisor.py
@@ -0,0 +1,46 @@
+
+# app/robo_advisor.py
+
+## Modules & Packages
+
+
+
+
+##
+## Input
+##
+
+# Intro
+print("=============================")
+print("WELCOME TO ROBO-ADVISOR!")
+
+# User Type-In
+print("-------------------------")
+symbol = input("Please input a stock symbol (e.g. MSFT, AAPL, AMZN): ")
+## TODO: Validate the symbol
+
+
+
+##
+## Output
+##
+
+print("-------------------------")
+print(f"SELECTED STOCK: {symbol}")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print("LATEST DAY: 2018-02-20")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
+
+
+
