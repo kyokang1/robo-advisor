875b22e753d4c50fb7bf0dd824c5465a88e0f7d9
kyokang1 <kyoungmokang@gmail.com>
2019-06-22 05:54:30 -0400

    pytest - get_response

--- test/robo_advisor_test.py
+++ test/robo_advisor_test.py
@@ -2,13 +2,18 @@
 
 import os
 
-from app.robo_advisor import to_usd
-
-#import sys
-#sys.path.append('../')
-#from shopping_cart import to_usd
+from app.robo_advisor import to_usd, get_response
 
 def test_to_usd():
     price = 4050.13
     assert to_usd(price) == "$4,050.13"
+
+def test_get_response():
+    symbol = "GOOG"
+    parsed_response = get_response(symbol)
+
+    assert "Meta Data" in parsed_response.keys()
+    assert "Time Series (Daily)" in parsed_response.keys()
+    assert parsed_response["Meta Data"]["2. Symbol"] == symbol
+
 
